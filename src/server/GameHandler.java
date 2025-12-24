30599cc3c080599e2c2472ee9a102cd160095c17
Alessio Cascini <cascini.alessio@studenti-ittfedifermi.edu.it>
2025-12-24 14:39:52 +0100

    refactor(GameHandler)

--- src/server/GameHandler.java
+++ src/server/GameHandler.java
@@ -7,8 +7,8 @@ public class GameHandler implements Runnable {
   private static final int[] sunkenShipsCount = {0, 0};
   private static boolean isPlayerOneTurn = true;
   private static boolean isFirstTurn = true;
-  private static int[] lastMove = null;
-  private static int[][] lastResult = null;
+  private static int[] move = null;
+  private static int[][] result = null;
   private final Socket clientSocket;
 
   public GameHandler(final Socket clientSocket) {
@@ -22,39 +22,37 @@ public class GameHandler implements Runnable {
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
       final boolean isPlayerOne = (Boolean) in.readObject();
-      if (isPlayerOne == isPlayerOneTurn) {
-        out.writeObject(true);
 
-        if (!isPlayerOne && isFirstTurn) {
-          out.writeObject(lastMove);
-          out.writeObject(lastResult);
+      out.writeObject(isPlayerOne == isPlayerOneTurn);
+      if (isPlayerOne != isPlayerOneTurn) return;
 
-          isFirstTurn = false;
-        }
+      if (!isPlayerOne && isFirstTurn) {
+        out.writeObject(move);
+        out.writeObject(result);
 
-        final int[] move = (int[]) in.readObject();
-        final int[][] result = Server.processMove(isPlayerOne, move);
+        isFirstTurn = false;
+      }
 
-        if (result.length > 1) sunkenShipsCount[isPlayerOne ? 1 : 0]++;
+      move = (int[]) in.readObject();
+      result = Server.processMove(isPlayerOne, move);
 
-        if (sunkenShipsCount[isPlayerOne ? 1 : 0] == Server.getShipCount()) {
-          final int[][] winMessage = new int[result.length + 1][2];
-          winMessage[0] = new int[] {-1, -1};
-          System.arraycopy(result, 0, winMessage, 1, result.length);
-          out.writeObject(winMessage);
-        } else out.writeObject(result);
+      if (result.length > 1) sunkenShipsCount[isPlayerOne ? 1 : 0]++;
 
-        lastMove = move;
-        lastResult = result;
+      if (sunkenShipsCount[isPlayerOne ? 1 : 0] < Server.getShipCount()) out.writeObject(result);
+      else {
+        final int[][] message = new int[result.length + 1][];
+        message[0] = new int[] {-1, -1};
+        System.arraycopy(result, 0, message, 1, result.length);
+        out.writeObject(message);
+      }
 
-        isPlayerOneTurn = !isPlayerOneTurn;
+      isPlayerOneTurn = !isPlayerOneTurn;
 
-        while (isPlayerOne != isPlayerOneTurn) Thread.sleep(100);
-        if (sunkenShipsCount[isPlayerOne ? 0 : 1] == Server.getShipCount())
-          out.writeObject(new int[] {-1, -1});
-        else out.writeObject(lastMove);
-        out.writeObject(lastResult);
-      } else out.writeObject(false);
+      while (isPlayerOne != isPlayerOneTurn) Thread.sleep(100);
+
+      if (sunkenShipsCount[isPlayerOne ? 0 : 1] < Server.getShipCount()) out.writeObject(move);
+      else out.writeObject(new int[] {-1, -1});
+      out.writeObject(result);
     } catch (Exception e) {
       e.printStackTrace();
     }
